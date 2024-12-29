a19d72c95f564337fced1979555641b3634d86bc
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2024-12-29 15:10:54 +0100

    Clase sesion cambiada

--- src/main/java/es/deusto/sd/auctions/client/data/Sesion.java
+++ src/main/java/es/deusto/sd/auctions/client/data/Sesion.java
@@ -3,6 +3,11 @@ package es.deusto.sd.auctions.client.data;
 
 
 public record Sesion( 
-	) {
-
-}
+		Long id,
+		String titulo,
+		String deporte,
+		Float distancia,
+		Long horaInicio,
+		Long horaFin,
+		Float duracion
+	) {}
